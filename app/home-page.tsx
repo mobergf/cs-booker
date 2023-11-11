9687f7c4a1d60b7755143a1c2de76bbd0e6e3d82
Freppa <freddan.moberg@gmail.com>
2023-11-11 13:55:55 +0100

    Updated index page sections to fit new design

--- app/home-page.tsx
+++ app/home-page.tsx
@@ -7,6 +7,8 @@ import { User } from "next-auth";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import ModeToggle from "./mode-toggle";
+import { MinusIcon } from "components/icons/MinusIcon";
+import { AddIcon } from "components/icons/AddIcon";
 
 const weekdays = [
   "Söndag",
@@ -146,7 +148,7 @@ const Page = ({
     );
   };
   return (
-    <div className="mx-auto mt-4 max-w-5xl pb-8 md:px-4">
+    <div className="mx-auto max-w-5xl pb-8 pt-4 md:px-4">
       <div className="flex flex-row items-center justify-between px-4 md:px-0">
         <h1 className="text-2xl font-bold md:text-4xl">HUVUDSKOTT.SE</h1>
         <ModeToggle />
@@ -161,24 +163,19 @@ const Page = ({
         return (
           <section
             key={ix + userMatchesState?.totalItems}
-            className={`m-2.5 mt-5 rounded-[3px] bg-white dark:bg-[#182535] dark:bg-opacity-50 dark:bg-gradient-to-tl dark:from-[#2F3F5C]/50 dark:via-[#022B31]/50 dark:to-[#214F73]/50 md:m-0 md:mt-6`}
+            className={`m-2.5 mt-5 rounded-[3px] bg-white bg-opacity-70 dark:bg-[#182535] dark:bg-opacity-50 dark:bg-gradient-to-tl dark:from-[#2F3F5C]/50 dark:via-[#022B31]/50 dark:to-[#214F73]/50 md:m-0 md:mt-6`}
           >
             <button
-              className="inline-flex h-16 w-full items-center  px-4 py-4 "
+              className="inline-flex w-full items-center px-4 py-4 "
               onClick={() =>
                 setOpenAccordion((prev) => (prev === ix ? 50 : ix))
               }
             >
-              <span className="flex flex-1 text-left">
-                <div className="flex">
-                  <h2 className="text-xl md:text-2xl">
-                    {`${weekdays[new Date(item).getDay()]} - ${item}`}
-                  </h2>
-                </div>
-              </span>
-
-              <div className="flex items-center  gap-3 md:gap-8">
-                <div className="flex gap-2 md:gap-4">
+              <span className="w-full">
+                <h2 className="flex text-xl md:text-2xl">
+                  {`${weekdays[new Date(item).getDay()]} - ${item}`}
+                </h2>
+                <div className="flex gap-4 pt-1 md:pt-2">
                   <div className="flex h-full  gap-1">
                     <CutleryIcon />
                     {dayUsers?.length}
@@ -188,23 +185,10 @@ const Page = ({
                     {nightUsers?.length}
                   </div>
                 </div>
+              </span>
 
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className={`h-6 w-6 ${
-                    openAccordion === ix ? "rotate-180" : "rotate-0"
-                  }`}
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                  />
-                </svg>
+              <div className="flex items-center  gap-3 md:gap-8">
+                {openAccordion === ix ? <MinusIcon /> : <AddIcon />}
               </div>
             </button>
             <div
@@ -213,18 +197,24 @@ const Page = ({
               } overflow-hidden`}
             >
               <div className="p-4 pt-0">
-                <div className="rounded-[3px] bg-zinc-200 p-4 dark:bg-black dark:bg-opacity-30">
+                <div className="rounded-[3px] bg-[#B3BDCD25] p-4 dark:bg-black dark:bg-opacity-30">
                   <div className="flex flex-row items-center justify-between">
-                    <h3 className="text-xl md:text-2xl">Lunchpang</h3>
+                    <h3 className="text-xl md:text-2xl">
+                      <CutleryIcon className="mr-1 inline-flex h-5 w-5" />
+                      Lunchpang
+                    </h3>
                     <ButtonFilter item={item} type="day" />
                   </div>
                   <DisplayList matches={dayUsers} />
                 </div>
               </div>
               <div className="p-4 pt-0">
-                <div className="rounded-[3px] bg-zinc-200 p-4 dark:bg-black dark:bg-opacity-30">
+                <div className="rounded-[3px] bg-[#B3BDCD25] p-4 dark:bg-black dark:bg-opacity-30">
                   <div className="flex flex-row items-center justify-between">
-                    <h3 className="text-xl md:text-2xl">Kvällspang</h3>
+                    <h3 className="text-xl md:text-2xl">
+                      <DayIcon className="mr-1 inline-flex h-5 w-5" />
+                      Kvällspang
+                    </h3>
                     <ButtonFilter item={item} type="night" />
                   </div>
                   <DisplayList matches={nightUsers} />
