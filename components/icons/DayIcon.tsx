62a81c39627004b9e73c2ba86a768e06008d23b7
Freppa <freddan.moberg@gmail.com>
2023-11-11 13:56:34 +0100

    Added icons and allowed passing className to icons

--- components/icons/DayIcon.tsx
+++ components/icons/DayIcon.tsx
@@ -1,11 +1,11 @@
-export const DayIcon = () => (
+export const DayIcon = ({ className }: { className?: string }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
     strokeWidth={1.5}
     stroke="currentColor"
-    className="h-6 w-6 "
+    className={`h-6 w-6 ${className}`}
   >
     <path
       strokeLinecap="round"
